e1bccf8731fcea1b6164babb5618b3dd58b4c4d3
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-14 14:30:19 -0700

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import App from './containers/App';
 
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Takes 3 parameters
+// the 1st is all of our reducers combined into 1
+// the 2nd is any state that we want pre loaded.
+// the 3rd is any middleware we want to use with redux
+
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
   , document.getElementById('root'));
