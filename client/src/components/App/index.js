0a675b6fde5c070572e654bafb7faf9fdcdc0fb8
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-14 12:54:38 -0700

    Init repo

---
+++ client/src/components/App/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default (props) => (
+  <div>
+    I am the app component!!!
+    { props.children }
+  </div>
+);
---
+++ routes/apiRoutes/userTodoRoutes/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getUserTodos,
+  deleteUserTodoById,
+  updateTodoById,
+  getAllUserEmails,
+} = require('../../../controllers/userController');
+
+const { requireAuth } = require('../../../middlewares/authMiddlewares');
+
+// /api/user/emails
+router.get('/emails', getAllUserEmails);
+
+// /api/user/todos
+
+
+router.route('/todos')
+  .get(requireAuth, getUserTodos);
+// /api/user/emails
+
+router.route('/todos/:todoId')
+  .delete(requireAuth, deleteUserTodoById)
+  .put(requireAuth, updateTodoById);
+
+module.exports = router;
