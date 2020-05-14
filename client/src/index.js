8f34d12f9b9e2ee948081972dea90ce95880852d
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-14 13:08:23 -0700

    Move app component into container

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './components/App';
+import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'));
