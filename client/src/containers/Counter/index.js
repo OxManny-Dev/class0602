4e478c39aa1e217de776f46b2278eca28307636c
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-15 10:53:52 -0700

    Connect counter state with counter container for increment

--- client/src/containers/Counter/index.js
+++ client/src/containers/Counter/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Header, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { increment } from '../../actions/counterActions';
 
 class Counter extends Component {
   render() {
@@ -11,7 +13,7 @@ class Counter extends Component {
             textAlign='center'
             content='Welcome to the Counter App'
           />
-          <Header as='h2' textAlign='center'>Counter: <span>0</span></Header>
+          <Header as='h2' textAlign='center'>Counter: <span>{this.props.counter}</span></Header>
           <Button.Group>
             <Button
               icon='minus circle'
@@ -24,6 +26,7 @@ class Counter extends Component {
               content='Increment'
               positive
               labelPosition='right'
+              onClick={this.props.increment}
             />
           </Button.Group>
         </Grid.Column>
@@ -32,4 +35,21 @@ class Counter extends Component {
   }
 }
 
-export default Counter;
+
+
+// connect takes 2 parameters.
+// The 1st one is a function that we should call mapStateToProps
+// The 2nd one, is mapDispatchToProps. This is going to be an object
+// That object is what action creators we want connected to this component
+function mapStateToProps(state) {
+  return { counter: state.counter };
+};
+
+// connect is a higher order component
+
+//What this will do
+// Is now inside of the counter component
+//I will have access to
+// this.props.counter
+// this.props.increment
+export default connect(mapStateToProps, { increment })(Counter);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import counterReducer from './counterReducer';
 
 export default combineReducers({
+  counter: counterReducer,
   form: formReducer
 });
