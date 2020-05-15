39f8ba5b448000869bc379cf10f24b711a7fb6db
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-15 10:34:44 -0700

    Create counter reducer

---
+++ client/src/reducers/counterReducer.js
@@ -0,0 +1,10 @@
+import { INCREMENT } from '../actions/types';
+
+export default function(state = 0, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    default:
+      return state;
+  }
+}
